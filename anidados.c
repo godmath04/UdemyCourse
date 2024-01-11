edc74325b1f3c1889248872b1d98a308a49ad8e6
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2024-01-11 15:42:35 -0500

    No se que cambio

--- anidados.c
+++ anidados.c
@@ -3,14 +3,17 @@ const int LINEAS = 10;
 const char BLANCO = ' ';
 const char CRUZ = 'x';
 
+/*PUTCHAR se utiliza para imprimir un solo caracter de salida estandar 
+Por otro lado PUTS imprime una cadena de caracteres seguida de un salto de linea*/
+
 int main(int argc, char const *argv[])
 {
     int fila, blancos, cruces;
     puts(" "); // comillas dobles porque es cadena de caracteres
 
-    for (fila = 1; fila < LINEAS; fila++)
+    for (fila = 1; fila <= LINEAS; fila++)
     {
-        putchar('\t'); // comillas simple porque son caracteres
+        //putchar('\t'); // comillas simple porque son caracteres
         for (blancos = LINEAS - fila; blancos > 0; blancos--)
         {
             putchar(BLANCO);
