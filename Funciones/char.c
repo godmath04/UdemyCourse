a6f1d5d276781eae34ecba992d95348a5624d237
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2024-01-11 23:53:40 -0500

    Mas texto

--- Funciones/char.c
+++ Funciones/char.c
@@ -7,5 +7,21 @@ int main(int argc, char const *argv[])
     printf("Por favor escribe tu nombre:\n");
     gets(nombre); // Se queda esperando hasta meterle el nombre
     printf("Hola %s tu nombre tiene %d letras.\n", nombre, strlen(nombre)); //
+
+
+    char c;
+    int i = 0;
+    do
+    {
+        printf("Ingrese un caracter: ");
+        c = getchar();
+        fflush(stdin);
+        printf("El caractrer ingresasddo es: ");
+        putchar(c);
+        putchar('\n');
+        putchar('\n');
+        i++;
+    } while (i<6);
+    
     return 0;
 }
