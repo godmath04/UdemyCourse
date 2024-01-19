28f41dea4b18d9ebd201e6acd4e6215e41ca8799
Luis Pineda <luis.pineda.revelo@udla.edu.ec>
2024-01-19 12:06:05 -0500

    Basico puntero

---
+++ PUNTEROS/puntero.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int numero = 20;
+    // Mostramos el valor
+    printf("El valor de la variable es %d.\n", numero);
+    // Direccion en memoria
+    printf("%p\n", &numero);
+    // Mostrar almecenamiento
+    printf("El amlmacenamiento es %d\n", sizeof(numero));
+    return 0;
+}
